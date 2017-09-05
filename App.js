d0cf0f60a0bdab7763e48c44121bbce6ee157ee9
James Eschrich <jaeschrich@gmail.com>
2017-09-05 13:29:16 -0400

    started working on the structure

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
 import {Card} from './src/Card';
+import { createStore } from 'redux';
+import { List, Map, fromJS } from 'immutable';
+
+
+let store = createStore(function reducer(state = fromJS({ cards: {}, currentCard: null }), action) {
+    switch (action.type) {
+        case 'LIKE':
+            let card = state.cards.get(action.id);
+            return state.set('cards', state.cards.set(action.id, card.set('liked', true)));
+    }
+});
+
 
 export default class App extends React.Component {
     render() {
-        let pic = {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
         return (
             <View style={styles.container}>
-                <View style={styles.toolbar}>
-                    <Card></Card>
-                </View>
+                <View style={styles.toolbar}></View>
+                <Card></Card>
             </View>
         );
     }
@@ -25,9 +33,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         marginLeft: "2%",
         marginRight: "2%",
+        flexDirection: 'row'
     },
     toolbar: {
         paddingTop: 20,
-        flexDirection: 'row'
     }
 });
--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,46 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Image, StatusBar} from 'react-native';
+import {StyleSheet, Text, View, Image, StatusBar, Button } from 'react-native';
 
 export class Card extends React.Component {
     render() {
         return (
             <View style={styles.card}>
                 <Text>Hello World this is a lot more text and its great and stuff.</Text>
+                <View style={styles.toolbar}>
+                    <Button style={styles.dislikeButton} onPress={this.onDislike.bind(this)} title={"Dislike"}></Button>
+                    <Button style={styles.likeButton} onPress={this.onLike.bind(this)} title={"Like"}></Button>
+                </View>
             </View>
         );
     }
+    onDislike() {
+        store.dispatch({
+            type: 'DISLIKE',
+            id: this.props.id
+        });
+    }
+    onLike() {
+        store.dispatch({
+            type: 'LIKE',
+            id: this.props.id
+        });
+    }
+
 }
 
 const styles = StyleSheet.create({
     card: {
         flex: 1,
         backgroundColor: "#CCC"
+    },
+    toolbar: {
+        flexDirection: "row",
+        justifyContent: "center"
+    },
+    dislikeButton: {
+        backgroundColor: "red"
+    },
+    likeButton: {
+        backgroundColor: "green"
     }
 });
